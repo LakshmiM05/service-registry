9959d984e31ba07b0a587f2b86891117a1cbb850
Lakshmi <lakshmigid@gmail.com>
2024-08-21 18:49:40 +0530

    commit

---
+++ src/test/java/com/ser/reg/ms/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ser.reg.ms.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
